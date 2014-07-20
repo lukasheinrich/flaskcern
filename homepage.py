6ae9103da3766d141d3e6735d23c6685f4f28fea
lukasheinrich <lukas@pb-d-128-141-230-196.cern.ch>
2014-07-21 01:29:48 +0200

    started on homepage with flask

---
+++ homepage.py
@@ -0,0 +1,13 @@
+import flask
+app = flask.Flask(__name__)
+
+import datetime
+
+@app.route("/")
+def homepage():
+    return flask.render_template('index.html',year=datetime.datetime.now().year)
+
+
+    
+if __name__ == "__main__":
+    app.run()
