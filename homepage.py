7d276f9cff72ad2d0cd394002259f08d3da85f68
lukasheinrich <lukas@pb-d-128-141-229-165.cern.ch>
2014-07-21 12:19:54 +0200

    top bar routing

--- homepage.py
+++ homepage.py
@@ -3,11 +3,14 @@ app = flask.Flask(__name__)
 
 import datetime
 
-@app.route("/")
+@app.route('/')
 def homepage():
+    print 'routing homepage'
     return flask.render_template('index.html',year=datetime.datetime.now().year)
 
-
-    
+@app.route('/<name>')
+def page(name):
+    return flask.render_template('{}.html'.format(name),year=datetime.datetime.now().year)
+        
 if __name__ == "__main__":
     app.run()
