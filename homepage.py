5006a31ded7efb7e5d37a6aee2981645a86c6dbc
lukasheinrich <lukas@pb-d-128-141-165-87.cern.ch>
2014-07-22 23:57:50 +0200

    just flask no homepage

--- homepage.py
+++ homepage.py
@@ -5,12 +5,7 @@ import datetime
 
 @app.route('/')
 def homepage():
-    print 'routing homepage'
     return flask.render_template('index.html',year=datetime.datetime.now().year)
-
-@app.route('/<name>')
-def page(name):
-    return flask.render_template('{}.html'.format(name),year=datetime.datetime.now().year)
         
 if __name__ == "__main__":
     app.debug = True
