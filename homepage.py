1a9006c769bf1930fcbe21e787371003ad4012d8
lukasheinrich <lukas@pb-d-128-141-230-61.cern.ch>
2014-07-23 11:49:57 +0200

    text

--- homepage.py
+++ homepage.py
@@ -3,11 +3,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return flask.redirect(flask.url_for('flasktutorial'))
-    
-@app.route('/flask')
-def flasktutorial():
     return flask.render_template('index.html')
-        
+            
 if __name__ == "__main__":
     app.run()
